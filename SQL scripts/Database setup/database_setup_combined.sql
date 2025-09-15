e83e89742d1122db98846bd354710eb35ac7ff03
Keyla Nyacyesa Bineza <k.nyacyesa@alustudent.com>
2025-09-16 11:17:36 +1300

    Updates

--- SQL scripts/Database setup/database_setup_combined.sql
+++ SQL scripts/Database setup/database_setup_combined.sql
@@ -1,7 +1,7 @@
-source "create_database.sql";
-source "Users_table.sql";
-source "User_roles.sql";
-source "User_role_assignment.sql";
-source "User_log.sql";
-source "Transactions.sql";
-source "Transaction_categories.sql";
+source create_database.sql;
+source Users_table.sql;
+source User_roles.sql;
+source User_role_assignment.sql;
+source User_log.sql;
+source Transactions.sql;
+source Transaction_categories.sql;
