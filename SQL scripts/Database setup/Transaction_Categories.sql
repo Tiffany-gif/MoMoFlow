3ee52aeb4e60584ecc4f7ba96c0023b18a4581a4
Keyla Nyacyesa Bineza <k.nyacyesa@alustudent.com>
2025-09-18 03:35:19 +1300

    Delete SQL scripts/Database setup directory

--- SQL scripts/Database setup/Transaction_Categories.sql
+++
@@ -1,6 +0,0 @@
-USE Momoflow;
-
-CREATE TABLE IF NOT EXISTS Transaction_Categories (
-    Category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(50) NOT NULL
-);
--- SQL scripts/Database setup/database_setup_combined.sql
+++
@@ -1,7 +0,0 @@
-source create_database.sql;
-source Users.sql;
-source User_role.sql;
-source Transaction_Categories.sql;
-source Transactions.sql;
-source User_role_assignment.sql;
-source User_log.sql;
